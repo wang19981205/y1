5c5b8d55e1c376236d0cf23c380c2a2ced867ff6
yyh <839343158@qq.com>
2020-05-28 20:26:25 +0800

    我是程序员B，提交代码

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("i am a ");//我是程序员b
+        System.out.println("i am a ");//我是程序员b ，就hi给4546546578
 
     }
 
