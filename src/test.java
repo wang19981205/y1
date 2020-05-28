e1eca13410d0dd376f03bfea6b8afa3f5bf0633c
10157 <1015770492@qq.com>
2020-05-28 20:15:09 +0800

    程序员A，第一次提交代码

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("I am angel");//我是程序员A，第一次提交代码
 
     }
 
