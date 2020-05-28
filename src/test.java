9ba3f50536b43d68ab21d54316dce56211ae7ff1
yyh <839343158@qq.com>
2020-05-28 20:17:59 +0800

    我是程序员B，提交代码

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("i am a ");//我是程序员b
 
     }
 
